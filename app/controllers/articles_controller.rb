77505227e51714cfdba9e24165fa8f3902575d15
Capablanc4 <w.santiago.zama@gmail.com>
2025-05-21 10:52:24 -0500

    Bootstrap makes this to easy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,41 +11,34 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    
   end
 
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render :new
-    end 
+    end
   end
 
   def update
-    
     if @article.update(article_params)
       flash[:alert] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render :edit
     end
-
   end
 
   def show
-      
   end
 
   def destroy
-
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path(params[:id])
-    
   end
 
 
@@ -58,4 +51,3 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)
     end
 end
-
--- app/javascript/controllers/index.js
+++
@@ -1,7 +0,0 @@
-// app/javascript/application.js
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromDirectory } from "@hotwired/stimulus-webpack-helpers"
-
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromDirectory(context))
