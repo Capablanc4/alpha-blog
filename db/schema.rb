a07b647563bf5b004768bbad537f338358663a56
Capablanc4 <w.santiago.zama@gmail.com>
2025-05-26 20:39:10 -0500

    Add admin and user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_23_004741) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_27_010130) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_23_004741) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "articles", "users"
